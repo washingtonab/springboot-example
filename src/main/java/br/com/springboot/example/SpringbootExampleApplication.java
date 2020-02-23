1430ad8b5acb15f4cfce4b1d19696dcc13f9301e
washingtonab <washington.ab@gmail.com>
2020-02-23 12:13:00 -0300

    first commit

--- src/main/java/br/com/springboot/example/SpringbootExampleApplication.java
+++ src/main/java/br/com/springboot/example/SpringbootExampleApplication.java
@@ -1,12 +1,22 @@
 package br.com.springboot.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootExampleApplication {
+public class SpringbootExampleApplication implements CommandLineRunner {
 
+	@Autowired
+	private MyComponent myComponent;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootExampleApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		myComponent.hello();
 	}
 }
